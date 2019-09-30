60b34396f1fd6d0ea7e54433bdc82e3b068e268e
Ryan Breuer <breuerryan@gmail.com>
2019-09-30 14:23:48 -0500

    Add proper permissions

--- vehiclestorage/src/main/java/haveric/vehicleStorage/settings/Storages.java
+++ vehiclestorage/src/main/java/haveric/vehicleStorage/settings/Storages.java
@@ -1,7 +1,10 @@
 package haveric.vehicleStorage.settings;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +26,28 @@ public class Storages {
 
         if (!entityTypes.contains(entityType)) {
             entityTypes.add(entityType);
+        }
+
+        String name = storage.getName();
+        Permission perm = Bukkit.getPluginManager().getPermission("vehiclestorage.create." + name);
+        if (perm == null) {
+            perm = new Permission("vehiclestorage.create." + name, PermissionDefault.TRUE);
+            perm.setDescription("Allows creation of " + name + " VehicleStorages");
+            Bukkit.getPluginManager().addPermission(perm);
+        }
+
+        perm = Bukkit.getPluginManager().getPermission("vehiclestorage.open." + name);
+        if (perm == null) {
+            perm = new Permission("vehiclestorage.open." + name, PermissionDefault.TRUE);
+            perm.setDescription("Allows opening of " + name + " VehicleStorage inventories");
+            Bukkit.getPluginManager().addPermission(perm);
+        }
+
+        perm = Bukkit.getPluginManager().getPermission("vehiclestorage.destroy." + name);
+        if (perm == null) {
+            perm = new Permission("vehiclestorage.destroy." + name, PermissionDefault.TRUE);
+            perm.setDescription("Allows player to destroy " + name + " VehicleStorages");
+            Bukkit.getPluginManager().addPermission(perm);
         }
     }
 
